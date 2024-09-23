25b35b813d423bfa3cb8db4ed8638e726e67baed
Sholehani Hafezi <10mariashol15@gmail.com>
2024-09-22 22:42:38 -0700

    added total counter + placeholder

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
 let trueCellCount = document.getElementById("rs-table").childElementCount;
 let rowCount = Number("0");
-let cellCount = Number("24");
+let cellCount = Number("23");
 let table = document.getElementById("rs-table");
 
 function addCell() {
-    if(cellCount < 24){
+    if(cellCount < 23){
         cellCount++;
 
         let tempCell = document.querySelectorAll('[data-value]');
         tempCell = tempCell[cellCount - 1];
         tempCell.removeAttribute("style");
         setCookie("cellCount", cellCount, 365);
+        setTotalPoints();
     }
 
 }
 function removeCell() {
     if(cellCount > 0){
+        if(cellCount >= 24) cellCount--;
+
         let tempCell = document.querySelectorAll('[data-value]');
         tempCell = tempCell[cellCount - 1];
         tempCell.setAttribute("style", "display: none");
 
         cellCount--;
         setCookie("cellCount", cellCount, 365);
+        setTotalPoints();
     }
 }
 
 
 function loadTable(){
-    // clearCookies();
+    if(getCookie("cellCount") >= 24){
+        clearCookies();
+    }
     if(getCookie("t1") == "") {
         setTable(99);
     }
@@ -35,7 +41,7 @@ function loadTable(){
         setTable(-1);
         let tempCount = +getCookie("cellCount");
 
-        for(let i = 24; i > tempCount; i--) {
+        for(let i = 23; i > tempCount; i--) {
             removeCell();
         }
         cellCount = tempCount;
@@ -44,66 +50,91 @@ function loadTable(){
 }
 
 function setTable(numerator) {
-    let totalCellCount = Number(0);
-    while(rowCount < 8) {
+    let totalCellCount = Number(1);
+    while(totalCellCount < 24) {
+        let tableCell = document.createElement("div");
+        tableCell.setAttribute("data-value", totalCellCount);
+        let imgInput = document.createElement("input");
+        let skillImg = document.createElement("label");
+        let skillFrac = document.createElement("div");
+        let numer = document.createElement("input");
+        let denom = document.createElement("p");
 
-        let rowContainer = document.createElement("div");
-        rowContainer.classList.add("rs-table-row");
-    
-        let tempCellCount = 0;
-        while(tempCellCount < 3) {
-            totalCellCount++;
+        let slider = document.createElement("input");
+        slider.type = "range";
+        slider.min = 0;
+        slider.max = 99;
+        slider.step = 1;
+        slider.classList.add("lv-slider");
 
-            let tableCell = document.createElement("div");
-            tableCell.setAttribute("data-value", totalCellCount);
-            let imgInput = document.createElement("input");
-            let skillImg = document.createElement("label");
-            let skillFrac = document.createElement("div");
-            let numer = document.createElement("input");
-            let denom = document.createElement("p");
-            tableCell.classList.add("inv-cell");
-            skillImg.classList.add("inv-img");
-            imgInput.classList.add("img-input");
-            skillFrac.classList.add("inv-fraction");
-            numer.classList.add("inv-num");
-            numer.type = "text";
-            numer.name = totalCellCount;
-            denom.classList.add("inv-den");
-    
-            skillImg.src = "media/images/heart-skill.jpg";
-            skillImg.alt = "Cell Img";
-            skillImg.name = totalCellCount;
-            imgInput.type = "file";
-            imgInput.id = "imgIn";
-            skillImg.setAttribute("for", "imgIn");
-            imgInput.addEventListener("click", loadImage(skillImg, ""));
+        tableCell.classList.add("inv-cell");
+        skillImg.classList.add("inv-img");
+        imgInput.classList.add("img-input");
+        skillFrac.classList.add("inv-fraction");
+        numer.classList.add("inv-num");
+
+        numer.type = "text";
+        numer.placeholder = 0;
+        numer.name = totalCellCount;
+        denom.classList.add("inv-den");
+
+        skillImg.src = "media/images/heart-skill.jpg";
+        skillImg.alt = "Cell Img";
+        skillImg.name = totalCellCount;
+        imgInput.type = "file";
+        imgInput.id = "imgIn";
+        skillImg.setAttribute("for", "imgIn");
+        imgInput.addEventListener("click", loadImage(skillImg, ""));
 
 
-
-            if(numerator < 0){
-                numer.value = getCookie("t" + totalCellCount);
-            }
-            else {
-                numer.value = 0;
-                setCookie("t"+totalCellCount, numer.value, 365);
-            }
-            numer.addEventListener('change', updateCookie);
-            denom.textContent = "99";
-
-    
-            skillFrac.appendChild(numer);
-            skillFrac.appendChild(denom);
-            tableCell.appendChild(imgInput);
-            tableCell.appendChild(skillImg);
-            tableCell.appendChild(skillFrac);
-    
-            rowContainer.appendChild(tableCell);
-            tempCellCount++;
-
+        if(numerator < 0){
+            numer.value = getCookie("t" + totalCellCount);
         }
-        table.appendChild(rowContainer);
+        else {
+            setCookie("t"+totalCellCount, numer.value, 365);
+        }
+        numer.addEventListener('change', updateCookie);
+        denom.textContent = "99";
+
+
+        skillFrac.appendChild(numer);
+        skillFrac.appendChild(denom);
+        tableCell.appendChild(imgInput);
+        tableCell.appendChild(skillImg);
+        tableCell.appendChild(skillFrac);
+        tableCell.appendChild(slider);
+        table.appendChild(tableCell);
+
         rowCount++;
+        totalCellCount++;
     }
+
+    if(totalCellCount == 24){
+        let tableCell = document.createElement("div");
+        tableCell.setAttribute("data-value", totalCellCount);
+
+        let pointCounter = document.createElement("p");
+        let points = document.createElement("p");
+
+        tableCell.id = "total-count";
+        tableCell.classList.add("inv-cell");
+        pointCounter.textContent = "Total Level:";
+        tableCell.appendChild(pointCounter);
+        tableCell.appendChild(points);
+        table.appendChild(tableCell);
+    }
+}
+
+function setTotalPoints() {
+    let total = Number(0);
+    let maxCells = Number(getCookie("cellCount"));
+    for(let i = 1; i <= maxCells; i++){
+        let tempPoints = getCookie("t" + i);
+        total += Number(tempPoints);
+    }
+    
+    let totalCell = document.getElementById("total-count");
+    let points = totalCell.childNodes[1].textContent = total;
 }
 
 function setCell(cell, img) {
@@ -131,6 +162,7 @@ function getCookie(cname) {
 }
 function updateCookie() {
     setCookie("t" + this.name, this.value, 365);
+    setTotalPoints();
 }
 
 // https://stackoverflow.com/questions/179355/clearing-all-cookies-with-javascript
@@ -154,6 +186,7 @@ function loadImage(imgLabel, img) {
 
 document.addEventListener('DOMContentLoaded', function() {
     loadTable();
+    setTotalPoints();
 
     let addButton = document.getElementById("add");
     addButton.onclick = addCell;
